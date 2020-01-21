2bc6aec5eb191f0f52e339a3802fb734dc33ec5f
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-01-20 23:23:19 -0500

    finished all

--- src/exercises.js
+++ src/exercises.js
@@ -117,40 +117,107 @@ function difference(numbers) {
 
 
   function everywhere(values, x) {
-      let flag = 1;
+      let tag = 1;
       if (!values || values.length < 1 || x === undefined) {
           return false;
       } else {
 
           for (let i = 0; i < values.length - 1; i++) {
               if (values[i] === x) {
-                  flag = 0;
+                  tag = 0;
               } else if (values[i - 1] === x || values[i + 1] === x) {
-                  flag = 0;
+                  tag = 0;
               } else {
-                  flag = 1;
+                  tag = 1;
                   return false;
               }
           }
       }
-      if (flag === 0) {
+      if (tag === 0) {
           return true;
       }
 
   }
 
-function consecutive(numbers) {
-  // write your code here
-}
+  function consecutive(numbers) {
+      if (!numbers || numbers.length === 0) {
+          return false;
+      }
 
-function balance(numbers) {
-  // write your code here
-  console.log("test");
-}
+      var len = numbers.length;
 
-function clumps(values) {
-  // write your code here
-}
+      if (len < 3) {
+          return false;
+      }
+      for (i = 0; i < len; i++) {
+          if (isNaN(numbers[i])) {
+              return false;
+          }
+      }
+      for (i = 0; i < numbers.length - 2; i++) {
+          if ((numbers[i] % 2 == 0) && (numbers[i + 1] % 2 == 0) && (numbers[i + 2] % 2 == 0)) {
+              return true;
+          }
+          if ((numbers[i] % 2 != 0) && (numbers[i + 1] % 2 != 0) && (numbers[i + 2] % 2 != 0)) {
+
+              return true;
+          }
+      }
+
+      return false;
+  }
+
+  function balance(numbers) {
+      if (!numbers || numbers.length === 0) {
+          return false;
+      }
+
+      var len = numbers.length;
+
+      if (len < 2) {
+          return false;
+      }
+      let lenSum = 0;
+
+      for (i = 0; i < len; i++) {
+          if (isNaN(numbers[i])) {
+              return false;
+          }
+          lenSum += numbers[i];
+      }
+      let rSum = 0;
+      for (i = len - 1; i >= 0; i--) {
+          rSum += numbers[i];
+          lenSum -= numbers[i];
+          if (lenSum == rSum) {
+              return true;
+          }
+      }
+      return false;
+  }
+
+  function clumps(values) {
+      if (!values) {
+          return -1;
+      }
+      let temperature = "";
+      let res = 0;
+      let final = "";
+
+      for (i = 0; i < values.length; i++) {
+
+          if (values[i] == temperature) {
+              if (final.length == 0) {
+                  res++;
+                  final = values[i];
+              }
+          } else {
+              final = '';
+          }
+          temperature = values[i];
+      }
+      return res;
+  }
 
 /*
  * Exports all functions for use in external grader.js file. Do not modify.
